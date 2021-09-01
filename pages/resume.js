1422e4464a66e28664cb34eecbdf968cd2027bd8
Inez Escandon <inez@poetic.io>
2021-09-01 16:03:29 -0500

    clean up code

--- pages/resume.js
+++ pages/resume.js
@@ -25,7 +25,7 @@ export default function Resume() {
           <div id="resume">
             <p className="mb-5 text-4xl font-semibold">Check out my <span className="text-5xl font-medium font-dawningOfaNewDay">resume</span></p>
             <div>{lorem}</div>
-            <div>DOWNDLOAD RESUME BUTTON</div>
+            {/* <div>DOWNDLOAD RESUME BUTTON</div> */}
           </div>
         </section>
       </Layout>
