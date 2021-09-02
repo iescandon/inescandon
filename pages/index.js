1db29dd58ae2ed014f10796198eb3ffd6342f47c
Inez Escandon <inez@poetic.io>
2021-09-02 15:06:22 -0500

    removed houston picture in mobile

--- pages/index.js
+++ pages/index.js
@@ -21,22 +21,31 @@ export default function Home() {
           isWork={false}
           selectedSection={""}
         >
-        <section className="p-10 w-full lg:w-9/12 lg:ml-auto">
-          <div id="work" className="md:grid md:grid-cols-2">
-            <p className="md:col-span-2 mb-5 text-4xl font-semibold">Hola! My name is <span className="text-5xl font-medium font-dawningOfaNewDay pr-1">Inez</span>. I'm a Full Stack Web Developer based out of Houston, TX. <img src="/images/texas.png" alt="texas icon" height="30px" width="30px" className="inline pb-1"></img></p>
-            <img src="/images/houston.jpg" alt="houston texas city skyline" className="w-full md:col-span-2 mb-10"></img>
-            <hr className="my-3 border-coolBlue border-t-2 col-span-2"></hr>
+        <section className="w-full p-10 lg:w-9/12 lg:ml-auto">
+          <div id="work" className="flex flex-col w-full md:grid md:grid-cols-2">
+            <p className="mb-5 text-4xl font-semibold md:col-span-2">Hola! My name is <span className="pr-1 text-5xl font-medium font-dawningOfaNewDay">Inez</span>. I'm a Full Stack Web Developer based out of Houston, TX. <img src="/images/texas.png" alt="texas icon" height="30px" width="30px" className="inline pb-1"></img></p>
+            <div className="flex justify-center w-full col-span-2">
+              <Link href="/">
+                <img
+                  className="w-2/4 mb-5 border-2 rounded-full cursor-pointe md:w-1/3 border-coolBlue lg:hidden"
+                  src="/images/inez-pic.png"
+                  alt="hispanic girl with curly hair and small yorkie dog"
+                ></img>
+              </Link>
+            </div> 
+            <img src="/images/houston.jpg" alt="houston texas city skyline" className="hidden w-full mb-10 lg:block md:col-span-2"></img>
+            <hr className="col-span-2 my-3 border-t-2 border-coolBlue"></hr>
             {work.map((item, i) => (
               <>
               <div className="my-auto md:col-span-1">
-                <p className="uppercase pt-3 my-5 md:pt-0 md:mt-0 md:mb-5 text-2xl font-semibold">{item.title}</p>
+                <p className="pt-3 my-5 text-2xl font-semibold uppercase md:pt-0 md:mt-0 md:mb-5">{item.title}</p>
                 <p>{item.description}</p>
               </div>
               <div key={item.slug} className="p-5 my-5 md:col-span-1">
                 <div className="container">
                     <img
                       id="project-image"
-                      className="rounded-md shadow-lg w-full image"
+                      className="w-full rounded-md shadow-lg image"
                       src={item.image.src}
                       alt={item.image.alt}
                       tabIndex={0}
@@ -47,15 +56,15 @@ export default function Home() {
                   }}
                     ></img>
                   <Link href={`/work/${item.slug}`}>
-                    <div className="overlay rounded-md cursor-pointer">
+                    <div className="rounded-md cursor-pointer overlay">
                         <div className="middle">
-                        <div className="text rounded-md md:transition md:duration-200 md:ease-in-out text-white bg-coolBlue md:hover:text-black md:hover:bg-goldenrod">See More</div>
+                        <div className="text-white rounded-md text md:transition md:duration-200 md:ease-in-out bg-coolBlue md:hover:text-black md:hover:bg-goldenrod">See More</div>
                     </div>
                     </div>
                   </Link>
                 </div>
               </div>
-              {i + 1 !== work.length ? <hr className="my-3 text-platinum col-span-2"></hr> : null }
+              {i + 1 !== work.length ? <hr className="col-span-2 my-3 text-platinum"></hr> : null }
               </>
             ))}
           </div>
