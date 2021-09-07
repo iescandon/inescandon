f3c2d32793d057ae9890fa18d3a97da8dd23842e
Inez Escandon <inez@poetic.io>
2021-09-07 17:56:40 -0500

    changed contact button color

--- components/contact-form/index.js
+++ components/contact-form/index.js
@@ -39,7 +39,7 @@ function MyForm() {
         <input className="border rounded border-platinum" id="email" type="email" name="email" required />
         <label className="mt-5" htmlFor="message">Message:</label>
         <textarea className="border rounded border-platinum" id="message" name="message"></textarea>
-        <button className="self-end w-1/4 p-3 mt-10 text-black rounded md:transition md:duration-200 md:ease-in-out md:hover:text-white md:hover:bg-coolBlue bg-goldenrod md:w-1/6 submit-btn" type="submit" disabled={serverState.submitting}>
+        <button className="self-end w-1/4 p-3 mt-10 text-white rounded md:transition md:duration-200 md:ease-in-out md:hover:text-black md:hover:bg-goldenrod bg-coolBlue md:w-1/6 submit-btn" type="submit" disabled={serverState.submitting}>
           Submit
         </button>
         {serverState.status && (
